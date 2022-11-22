3bfc498aab04f36fac5c2bb4a3018e99b2472ddf
RominaMalimban <romina.lyn@hotmail.it>
2022-11-22 18:02:00 +0100

    milestone 1 completato

--- js/script.js
+++ js/script.js
@@ -40,33 +40,51 @@ let wrapper = document.querySelector(".items-container");
 
 
 images.forEach((element) => {
-
-    let img = element.image;
-    let titolo = element.title;
-    let descrizione = element.text;
     
     let item = ` <div class="item">
-                    <img src="${img}">
+                    <img src="${element.image}">
                     <div class="testo">
-                        <div>${titolo}</div>
-                        <div>${descrizione}</div>
+                        <div class="titolo">${element.title}</div>
+                        <div>${element.text}</div>
                     </div>
               
                 </div>`
 
-    
     wrapper.innerHTML += item;
-    
-   
 });
 
+// creo una variabile e ci salvo i div item e associo alla prima immagine la classe active:
+let divItem = document.getElementsByClassName("item");
+divItem[0].classList.add("active");
 
+// creo variabile dell'indice:
+let activeItem = 0;
 
+// creo le variabili per i bottoni next e prev:
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
 
+// creo funzione per click su bottone next:
+next.addEventListener("click",
+    function(){
 
+        // creo la condizione per passare la classe active: 
+        if(activeItem < divItem.length -1){
+            divItem[activeItem].classList.remove("active");
+            activeItem++;
+            divItem[activeItem].classList.add("active");
+        };
+        // nascondo il bottone next:
+        if(activeItem == divItem.length - 1){
+            next.style.display = "none";
+        };
 
-
-
+        // faccio comparire il bottone prev:
+        if(activeItem > 0){
+            prev.style.display = "block";
+        }
+    }
+); 
 
 // Milestone 2:
 // Aggiungere il **ciclo infinito** del carosello. Ovvero se l’img attiva è la prima e l’utente clicca la freccia prev, l’img che deve attivarsi sarà l’ultima e viceversa per l’ultima img se l’utente clicca la freccia next.
